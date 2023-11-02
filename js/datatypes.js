b0411824e644af7e83e9ccf13c0867ee4eebc22f
aviralchauhan <45326451+aviralchauhan@users.noreply.github.com>
2023-11-02 16:54:13 +0000

    h

---
+++ js/datatypes.js
@@ -0,0 +1,12 @@
+//primative data types
+// string
+// number
+// boolean
+// null
+// undefined
+// object
+// bigInt
+// symbol
+//console.log(typeof null);  null is object
+// console.log(typeof undefined) its type is undefined
+console.log(typeof Symbol);
---
+++ js/variables.js
@@ -0,0 +1,15 @@
+// const a=12234
+// let b="aviral"
+// var c="tabline"
+// d="date"
+// let ad=12
+
+// let e="12"
+// const ab="avi"
+// console.table([a,b,c,d,e,ab])
+// console.log(typeof(a));
+// console.table([typeof(a),typeof(b),typeof(c),typeof(d),typeof(e),typeof(ad)])
+let a=2
+console.log(a,typeof(a));
+a="aviral"
+console.log(a,typeof(a));
