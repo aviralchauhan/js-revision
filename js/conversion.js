407893f0415f15ab596692495f8bf542536361bf
aviralchauhan <45326451+aviralchauhan@users.noreply.github.com>
2023-11-02 18:56:43 +0000

    conversion

---
+++ js/conversion.js
@@ -0,0 +1,25 @@
+let a=undefined
+num = Number(a)
+// console.log(typeof num)
+// console.log(num)
+// conversion to number
+// "33" => 33
+// 22abc => NaN
+// null => 0
+// undefined => NaN
+// true => 1  
+
+
+let isLoggedIn="aviral"
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn)
+
+// 1 => true
+// 0 => false
+// NaN => false
+// aviral => true
+// "" => false
+
+// similarly things can be converted to string as well
+
+
