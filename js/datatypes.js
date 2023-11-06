62efb2761739431a9c00cacf7416f8de966e8582
aviralchauhan <45326451+aviralchauhan@users.noreply.github.com>
2023-11-06 20:47:36 +0000

    datatypes

--- js/datatypes.js
+++ js/datatypes.js
@@ -1,4 +1,7 @@
 //primative data types
+
+// these datatypes are call by value types
+
 // string
 // number
 // boolean
@@ -9,4 +12,27 @@
 // symbol
 //console.log(typeof null);  null is object
 // console.log(typeof undefined) its type is undefined
-console.log(typeof Symbol);
+// console.log(typeof Symbol);
+
+// Non primative data types (reference types)
+// array objects functions
+
+// *************************************************************************
+// types of memory
+// stack ->used in primaitve data types
+// heap -> used in non primative data types
+
+// let heros="ironman";
+// let marvel=heros;
+// marvel="hulk"
+// console.log(heros);
+// console.log(marvel);
+
+let myObj={
+    name:"aviral",
+    age:20
+}
+let anotherObj=myObj;
+anotherObj.name="bhuhh"
+console.log(myObj);
+console.log(anotherObj);
