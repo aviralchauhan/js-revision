15926110e7d45258df7f691a180c3dd143818e89
aviralchauhan <45326451+aviralchauhan@users.noreply.github.com>
2023-11-15 03:13:32 +0000

    arrays

--- js/array.js
+++ js/array.js
@@ -1,7 +1,7 @@
-const myArr=[1,2,3,4,5];
-const heros=["iromman","hulk",]
+// const myArr=[1,2,3,4,5];
+// const heros=["iromman","hulk",]
 
-const myArr2=new Array(1,2,3,4,5);
+// const myArr2=new Array(1,2,3,4,5);
 
 //**  push pop shift unshift
 
@@ -33,3 +33,29 @@ const myArr2=new Array(1,2,3,4,5);
 
 // learn difference b/w slice and splice
 
+// const marvel=["thor","ironman","spiderman"];
+// const dc=["superman","flash","batman"];
+
+// marvel.push(dc);
+// console.log(marvel);
+// marvel.push(...dc);
+// console.log(marvel);
+// ... -> this is spread operator
+
+// const all=marvel.concat(dc); returns new array
+// console.log(all);
+
+// const newArr=[1,2,3,[4,5,6],7,[8,9,10]];
+// console.log(newArr);
+// const newArr2=newArr.flat(Infinity);
+// console.log(newArr2);
+
+// console.log(Array.isArray("Aviral"));
+// console.log(Array.from("Aviral")); convert anything to an array
+// check array from object
+
+// let score1=100;
+// let score2=90;
+// let score3=80;
+
+// console.log(Array.of(score1,score2,score3)); // convert multiple elements to an array
