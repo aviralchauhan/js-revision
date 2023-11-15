1e4563a57b78c2f988a095fbde201059438bb4e2
aviralchauhan <45326451+aviralchauhan@users.noreply.github.com>
2023-11-15 02:05:24 +0000

    arrays

---
+++ js/array.js
@@ -0,0 +1,35 @@
+const myArr=[1,2,3,4,5];
+const heros=["iromman","hulk",]
+
+const myArr2=new Array(1,2,3,4,5);
+
+//**  push pop shift unshift
+
+// console.log(myArr);
+// myArr.push(18);
+// myArr.push("laptop");
+// console.log(myArr);
+// myArr.pop();
+// console.log(myArr);
+// myArr.unshift("aviral");
+// console.log(myArr);
+// myArr.shift();
+// console.log(myArr);
+
+// const newArr=myArr.join("+");
+// console.log(newArr);
+
+
+// slice splice
+// const myn1=myArr.slice(1,3);
+// console.log("A ",myArr);
+// console.log(myn1);
+// console.log("B ",myArr);
+
+// const myn2=myArr.splice(1,3);
+// console.log(myn2);
+// console.log("C ",myArr);
+
+
+// learn difference b/w slice and splice
+
